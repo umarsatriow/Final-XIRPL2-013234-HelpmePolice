f03b5867a441142d375af22cce7a2ea3180b70cc
umar <https://gitlab.com/umarsatriow/HelloDebugLog11012018.git>
2018-05-03 15:50:51 +0700

    First Commit

---
+++ app/src/main/java/id/sch/smktelkom_mlg/afinal/xirpl2013234/helpmepolice/MainActivity.java
@@ -0,0 +1,13 @@
+package id.sch.smktelkom_mlg.afinal.xirpl2013234.helpmepolice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
